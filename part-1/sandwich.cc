4a3980947fe59c8ea4259da7a1960d7a6c71fc3b
Brandon Trujillo <btrujillo118@csu.fullerton.edu>
2023-10-16 20:37:18 -0700

    used vector command to declare 3 arguments to make sandwhich and debugged.

--- part-1/sandwich.cc
+++ part-1/sandwich.cc
@@ -1,4 +1,7 @@
-// TODO: Add the required header
+// Brandon Trujillo
+// btrujillo118@csu.fullerton.edu
+// @brandont118
+// Partners: @SirBagelton @pachmi
 
 #include <iostream>
 #include <string>
@@ -7,17 +10,24 @@
 int main(int argc, char* argv[]) {
   std::vector<std::string> arguments(argv, argv + argc);
 
-  // TODO: Validate that the number of arguments is correct.
+  // x: Validate that the number of arguments is correct.
   // If not, print an error message and return a non-zero value.
-
-  // TODO: Declare three std::string variables to hold the
+  if (arguments.size() != 4) {
+    std::cout << "error: you must supply three arguments\n";
+    return -1;
+  }
+  // x: Declare three std::string variables to hold the
   // protein, bread, and condiment input.
   // Initialize each variable with an element of the arguments vector
   // declared above.
-
-  // TODO: Use cout to print output following the pattern
+  std::string protein{arguments.at(1)};
+  std::string bread{arguments.at(2)};
+  std::string condiment{arguments.at(3)};
+  // x: Use cout to print output following the pattern
   // Your order: A *PROTEIN* sandwich on *BREAD* with *CONDIMENT*.
   // on its own line.
-
+  std::cout << "Your order: \n";
+  std::cout << " A " << protein << " sandwich on " << bread << " with "
+            << condiment << ".\n";
   return 0;
 }
