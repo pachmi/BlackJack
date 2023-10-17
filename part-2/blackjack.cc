541e40278581c5d04a047f58be468a980e066249
Joseph <74332055+SirBagleton@users.noreply.github.com>
2023-10-16 21:44:30 -0700

    Done

--- part-2/blackjack.cc
+++ part-2/blackjack.cc
@@ -1,4 +1,7 @@
-// TODO: Add the required header
+// Joseph Geiger
+// josgeig@csu.fullerton.edu
+// @SirBagelton
+// Partners:@brandont118, @pachmi
 
 #include <iostream>
 #include <string>
@@ -9,22 +12,28 @@
 int main(int argc, char* argv[]) {
   std::vector<std::string> arguments(argv, argv + argc);
 
-  // TODO: Validate that the number of arguments is correct.
+  // Validate that the number of arguments is correct.
   // If not, print an error message and return a non-zero value.
-
-  // TODO: declare a variable to hold the first card name, and initialize
+  if (arguments.size() != 3) {
+    std::cout << "error: you must supply two arguments\n";
+    return 1;
+  }
+  // declare a variable to hold the first card name, and initialize
   // the variable with the first command line argument
-
-  // TODO: declare a variable to hold the second card name, and initialize
+  std::string first{arguments.at(1)};
+  // declare a variable to hold the second card name, and initialize
   // the variable with the second command line argument
-
-  // TODO: if either card name is not valid, print out an error message and
+  std::string second{arguments.at(2)};
+  // if either card name is not valid, print out an error message and
   // return a non-zero value.
-
-  // TODO: declare a variable to hold the total score, and initialize
+  if (!IsCardName(first) || !IsCardName(second)) {
+    std::cout << "error: invalid card name";
+    return 1;
+  }
+  // declare a variable to hold the total score, and initialize
   // the variable with the result of calling the TwoHandScore function
-
-  // TODO: write a cout statement that prints the total score on its own line
-
+  int total{TwoCardHandScore(first, second)};
+  // write a cout statement that prints the total score on its own line
+  std::cout << total;
   return 0;
 }
